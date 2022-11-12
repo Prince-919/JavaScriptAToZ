73a6018cf7bfaf156096cc2902fb409a92ff9bb9
Prince-919 <86801714+Prince-919@users.noreply.github.com>
2022-11-11 21:03:17 -0800

    Object Overview

---
+++ 2-objectOverview/app.js
@@ -0,0 +1,30 @@
+// Single Line comment
+// Object Overview
+
+/************ Multi Line Comment *************/
+/* 
+Student 
+    id 
+    name
+    age 
+    section 
+*/
+
+// Create Object 
+const student = {
+    id : 01,
+    name : "abc",
+    age : 20,
+    sec : "A",
+}
+
+// Data seen on console
+
+console.log("All Data..");
+console.log(student);   
+
+console.log("One object data access");
+console.log(student.id);
+console.log(student.name);
+console.log(student.age);
+console.log(student.sec);
