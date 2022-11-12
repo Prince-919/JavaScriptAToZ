4df9eb68e1c651a70a6d9b752b6531fd6fc7d264
Prince-919 <86801714+Prince-919@users.noreply.github.com>
2022-11-12 07:24:08 -0800

    DataTypes in Javascript

---
+++ 4-DataTypes/index.js
@@ -0,0 +1,52 @@
+
+// 1. Primitive data types
+
+// 1. Number
+// let age = 20;
+// let temp = -25;
+// let price = 50.50;
+
+// 2. String
+// let name = 'ABS';
+
+// 3. Boolean
+// true / false
+
+// let isLoggedIn = true;
+// let hasPermission = false;
+
+// 4. undefined
+
+// let name;
+// console.log(name); // undefined
+
+// 5. BigInt
+// console.log(Number.MAX_SAFE_INTEGER); // 9007199254740991
+// Use Formula
+// console.log(2 ** 53 - 1);   // 9007199254740991
+
+// 6. Symbol
+// Unique
+
+
+
+// 2. Structural Type
+
+// 1. Object
+    // 1.1 Function
+    // Non-data structure
+    // callable
+
+    // 1.2 Arrays
+    // collection of values
+    // const numbers = [2,3,4, "Javascript", "Python", {name : "Prince"}]
+
+    // 1.3 Maps
+    
+    // 1.4 Set 
+
+    // 1.5 Date
+
+// 3. Structural root
+
+// 1. Null
