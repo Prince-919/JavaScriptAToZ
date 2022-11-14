fbdaae43053611db98ad0149421829cd7512b341
Prince-919 <86801714+Prince-919@users.noreply.github.com>
2022-11-14 03:52:57 -0800

    create Function with practice and variable scope

---
+++ 7-Functions/index.js
@@ -0,0 +1,45 @@
+
+// Functions
+// function 1
+// function login(username, password)
+ 
+// {
+//     console.log(`${username} , Logged in SuccessFully`);
+// }
+// login('Prince Sharma', 'secret');
+// login('Aripta Sharma', 'secret');
+
+// function 2
+// toUpperCase()
+
+// function upperCase(str){
+//     // console.log(str.toUpperCase());
+//     return str.toUpperCase();
+// }
+// // upperCase("Prince Sharma");
+// const result = upperCase("prince Sharma");
+// console.log(result);
+
+
+// function 3
+
+// function calculateArea(width, height)
+// {
+//     const area = width * height;
+//                     // 30 * undefined = 
+//     return area;
+// }
+// const area = calculateArea(30,40);
+// console.log(area);
+
+// function 4
+// variable scope 
+
+// const fileName = 'xyz.pdf';
+// function download()
+// {
+//     // const fileName = 'xyz.pdf';
+//     console.log(fileName);
+// }
+// // console.log(fileName);  //ReferenceError: fileName is not defined
+// download();
