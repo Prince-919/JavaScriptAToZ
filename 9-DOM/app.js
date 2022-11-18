d4de8334a186a74f8e956c5d080b6e003bfd2830
Prince-919 <86801714+Prince-919@users.noreply.github.com>
2022-11-17 21:29:52 -0800

    DOM access html element document.getElement

---
+++ 9-DOM/app.js
@@ -0,0 +1,31 @@
+
+// 1. getElementById
+
+// const heading = document.getElementById('heading');
+// console.log(heading);
+
+// 2. getElementsByTagName
+// const heading = document.getElementsByTagName('h1');
+// console.log(heading);
+// console.log(heading[0]);
+// console.log(heading[1]);
+
+
+// 3. getElementsByClassName
+// const para = document.getElementsByClassName('para');
+// console.log(para);
+// console.log(para[0]);
+// console.log(para[1]);
+
+// 4. querySelector
+
+// const heading = document.querySelector('#heading');
+// const heading1 = document.querySelector('.heading');
+// console.log(heading);
+// console.log(heading1);
+
+
+// 5. querySelectorAll
+const heading = document.querySelectorAll('.heading');
+console.log(heading);
+
